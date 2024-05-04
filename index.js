c531b4fa3c1a14a7a2e2309cdbf3f23b0e798e1a
anzilafatima <anzilafatima205@gmail.com>
2024-05-04 23:18:29 +0500

    word-counter

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:",
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+console.log(words);
+console.log(`your sentence words counts ${words.length}`);
---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node 
+import inquirer from  "inquirer";
+const answer: {
+    sentence: string 
+} = await inquirer.prompt([
+        {
+            name:"sentence",
+            type:"input",
+            message:"Enter your sentence to count the word:",
+        }
+    ]);
+
+    const words = answer.sentence.trim().split(" ")
+    console.log(words);
+    console.log(`your sentence words counts ${words.length}`);
+    
+    
+
